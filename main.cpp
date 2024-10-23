934b9a79c17452c52e3ac4c167e8b79c1b4e1d6f
frostie <mattwiner04@gmail.com>
2024-10-22 19:11:31 -0700

    2

---
+++ main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Matthew Winer on 10/22/24.
+//
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl;
+    return 0;
+}
