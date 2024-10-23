69a0c282a253b5c586c62e5a86fc11e35626cfa7
frostie <mattwiner04@gmail.com>
2024-10-22 21:08:44 -0700

    got audio working

--- main.cpp
+++ main.cpp
@@ -1,11 +1,37 @@
-//
-// Created by Matthew Winer on 10/22/24.
-//
-#include <stdio.h>
-#include <iostream>
-using namespace std;
-
+#include "raylib.h"
+#include <string>
 int main() {
-    cout << "Hello World!" << endl;
+    // Initialize the window and audio device
+    InitWindow(800, 450, "Simple Audio Player with Raylib");
+    InitAudioDevice();  // Initialize the audio device
+
+    Sound sound = LoadSound("../misato.wav");  // You can replace this with your own audio file
+
+    // Main game loop
+    while (!WindowShouldClose()) {
+        // Play sound on spacebar press
+        if (IsKeyPressed(KEY_SPACE)) {
+            PlaySound(sound);
+        }
+
+        // Stop sound on enter press
+        if (IsKeyPressed(KEY_ENTER)) {
+            StopSound(sound);
+        }
+
+        // Begin drawing
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText(sound,120,150,20,GREEN);
+        DrawText("Press SPACE to play sound, ENTER to stop", 120, 200, 20, DARKGRAY);
+
+        EndDrawing();
+    }
+
+    // Unload sound and close audio device
+    UnloadSound(sound);
+    CloseAudioDevice();  // Close the audio device
+    CloseWindow();       // Close window
+
     return 0;
 }
