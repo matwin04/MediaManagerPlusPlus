a56f24b5dd94f94658fab390c08d403a67755285
frostie <mattwiner04@gmail.com>
2024-10-23 11:41:20 -0700

    changed to qt UI

--- main.cpp
+++ main.cpp
@@ -1,60 +1,30 @@
-#include "raylib.h"
-#define RAYGUI_IMPLEMENTATION
-#include "raygui.h"
-#include <string>
-#include <iostream>
-using namespace std;
-float controlPitch(float pitch) {
-    static bool pitchEditMode = false;
-    static int pitchValue = (int)(pitch*50);
-    if (GuiSpinner((Rectangle){ 350, 200, 120, 24 }, "Pitch", &pitchValue, 10, 200, pitchEditMode)) {
-        pitchEditMode = !pitchEditMode;
-    }
-    return pitchValue / 50.0f;
-}
-void progBar(float progress) {
-    GuiProgressBar((Rectangle){10,250,750,34},NULL, NULL, &progress, 0.0f, 1.0f);
-}
+#include <QApplication>
+#include <QMainWindow>
+#include <QWidget>
+#include <QFileDialog>
+#include <QMediaPlayer>
+#include <QAudioOutput>
+#include "ui_audioplayer.h"
 int main() {
-    // Initialize the window and audio device
-    InitWindow(800, 450, "Simple Audio Player with Raylib");
-    InitAudioDevice();  // Initialize the audio device
-    Music music = LoadMusicStream("../misato.wav"); // Load File
+    int argc = 0;                    // Define a dummy argc
+    char *argv[] = { nullptr };
+    QApplication app(argc,argv);// Define a dummy argv
 
-    float pitch = 1.0f;
-    float progress = 0.0f;
+    QWidget window;
+    Ui::AudioPlayer ui;
+    ui.setupUi(&window);
+    QMediaPlayer *player = new QMediaPlayer();
+    QAudioOutput *audioOutput = new QAudioOutput();
+    player->setAudioOutput(audioOutput);
 
-    PlayMusicStream(music);
-    SetTargetFPS(60);
-    while (!WindowShouldClose()) {
-        UpdateMusicStream(music);
+    QObject::connect(ui.playButton, &QPushButton::clicked, player, &QMediaPlayer::play);
+    QObject::connect(ui.pauseButton, &QPushButton::clicked, player, &QMediaPlayer::pause);
+    QObject::connect(ui.stopButton, &QPushButton::clicked, player, &QMediaPlayer::stop);
 
-        //Pitch Related
-        pitch = controlPitch(pitch);
-        //Set Music
-        SetMusicPitch(music, pitch);
-        // get progress
-        progress = GetMusicTimePlayed(music) / GetMusicTimeLength(music);  // Progress from 0.0 to 1.0
+    QObject::connect(ui.volumeSlider, &QSlider::valueChanged, [=](int value){
+         audioOutput->setVolume(value / 100.0);  // Volume is a float between 0.0 and 1.0
+     });
+    window.show();
+    return app.exec();
 
-        // Control play/pause with SPACE key
-        if (IsKeyPressed(KEY_SPACE)) {
-            if (IsMusicStreamPlaying(music)) PauseMusicStream(music);
-            else ResumeMusicStream(music);
-        }
-        if (IsKeyPressed(KEY_ENTER)) StopMusicStream(music);
-
-        BeginDrawing();
-        ClearBackground(BLACK);
-        char pitchText[64];
-        sprintf(pitchText, "Current Pitch: %.2f", pitch);
-        DrawText(pitchText, 20, 20, 30, MAROON);
-        progBar(progress);
-        EndDrawing();
-    }
-
-    // Unload sound and close audio device
-    UnloadMusicStream(music);
-    CloseAudioDevice();  // Close the audio device
-    CloseWindow();       // Close window
-    return 0;
 }
